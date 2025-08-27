136714c9ecf3c4fd4ef3041c19a85ac303c27b48
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-08-27 21:49:23 +0100

    Writes test case

--- src/main.cpp
+++ src/main.cpp
@@ -1,37 +1,25 @@
 
 
 #include <chrono>
-#include <cstdint>
-#include <functional>
 #include <iostream>
 #include <thread>
 
-class AnoKernel {
- public:
-  AnoKernel() : n(0) {}
-  ~AnoKernel() {}
-
-  uint32_t introduce_func(std::function<uint32_t(AnoKernel &)> &&func) {
-    std::thread t([&]() { func(*this); });
-    t.detach();
-    return 0;
-  }
-
-  uint16_t n;
-};
+#include "AnoEnvironment.h"
+#include "AnoInterface.h"
+#include "Flags.h"
 
 int main() {
-  AnoKernel kern;
+  AnoEnvironment env;
 
-  kern.introduce_func([&](AnoKernel &ano_kern) {
-    std::this_thread::sleep_for(std::chrono::milliseconds(1000));
+  auto ano_thread = env.introduce<NewThread>([&](AnoInterface& interface) {
+    auto id = interface.get_this_id();
+    std::cout << "My id is: " << (int)id << std::endl;
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
 
-    std::cout << (int)ano_kern.n << std::endl;
-
-    return 1;
+    return NewThread();
   });
 
-  std::this_thread::sleep_for(std::chrono::milliseconds(1500));
+  //env.join(ano_thread->id());
 
   return 0;
 }
