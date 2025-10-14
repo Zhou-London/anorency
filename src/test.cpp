3907ef365f5302871671831d83bed4d56d25d405
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-10-14 20:14:52 +0100

    Updates the test cases

--- src/test.cpp
+++ src/test.cpp
@@ -1,16 +1,57 @@
-
-
+#include <exception>
 #include <iostream>
+#include <string>
 
-#include "AnoTaskDefault.h"
+#include "Aggregator.h"
+#include "AnoException.h"
+#include "Environment.h"
+#include "Line.h"
 
-int main(int argc, char **argv) {
-  auto task = Anorency::AnoTaskDefault();
+inline void TEST_CASE_EXCEPTION() {
+  std::cout << "\nStart Test: Exception.\n";
 
-  task.load([&]() { 
-    std::cout << "Hello World\n";
-    std::this_thread::sleep_for(std::chrono::seconds(1));
+  try {
+    throw Anorency::AnoException("Unknown Error.");
+  } catch (std::exception& e) {
+    std::cout << e.what() << "\n";
+  }
+}
+
+inline void TEST_CASE_ENVIRONMENT_INTRODUCE() {
+  std::cout << "\nStart Test: Environment_Introduce.\n";
+
+  Anorency::Environment env;
+  env.introduce([&](Anorency::AnoInterface&& iface) {
+    iface.set_str("First set.");
+    std::cout << iface.get_str() << "\n";
+
+    iface.set_str("Second set.");
+    std::cout << iface.get_str() << "\n";
   });
+}
 
+inline void TEST_CASE_AGGREGATOR() {
+  std::cout << "\nStart Test: Aggregator.\n";
+
+  Anorency::Aggregator aggr;
+  Anorency::Line<int> il;
+  il.push(1);
+  Anorency::Line<std::string> sl;
+  sl.push("Hello");
+
+  aggr.add(&il);
+  aggr.add(&sl);
+
+  auto v1 = aggr.get<int>(0);
+  auto v2 = aggr.get<std::string>(1);
+
+  std::cout << v1->peek() << "\n";
+  std::cout << v2->peek() << "\n";
+}
+
+int main() {
+  TEST_CASE_ENVIRONMENT_INTRODUCE();
+  TEST_CASE_EXCEPTION();
+  TEST_CASE_AGGREGATOR();
   return 0;
 }
