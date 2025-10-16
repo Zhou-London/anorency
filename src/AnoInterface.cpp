f19912535f22c62b116e38b02f637d6e0a4fb66a
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-10-16 11:30:20 +0100

    Supports Aggr.

--- src/AnoInterface.cpp
+++ src/AnoInterface.cpp
@@ -4,7 +4,8 @@
 
 namespace Anorency {
 
-AnoInterface::AnoInterface(std::string& str) : str(str) {}
+AnoInterface::AnoInterface(std::string& str, Aggregator& aggr)
+    : str(str), aggr(aggr) {}
 
 void AnoInterface::set_str(const std::string& new_str) { this->str = new_str; }
 
--- src/AnoInterface.h
+++ src/AnoInterface.h
@@ -1,18 +1,37 @@
 #pragma once
 
 #include <string>
+
+#include "Aggregator.h"
+#include "Types.h"
 namespace Anorency {
 
 class AnoInterface {
  public:
-  AnoInterface(std::string& str);
+  AnoInterface(std::string& str, Aggregator& aggr);
   ~AnoInterface() = default;
 
   void set_str(const std::string& new_str);
   std::string get_str();
 
+  template <typename T>
+  line_id_t make_line() {
+    return aggr.add<T>();
+  }
+
+  template <typename T>
+  line_id_t make_line(Line<T>* p) {
+    return aggr.add<T>(p);
+  }
+
+  template <typename T>
+  Line<T>* get_line(size_t index) {
+    return aggr.get<T>(index);
+  }
+
  private:
   std::string& str;
+  Aggregator& aggr;
 };
 
 }  // namespace Anorency
