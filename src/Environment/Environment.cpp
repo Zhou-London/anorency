c33cb53741ae10092b2b88adafc96c8f2b8f2763
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-10-16 11:30:54 +0100

    Supports aggr in env

---
+++ src/Environment/Environment.cpp
@@ -0,0 +1,13 @@
+#include "Environment.h"
+
+#include <cstddef>
+
+#include "Aggregator.h"
+
+namespace Anorency {
+
+Environment::Environment(std::size_t aggr_size) : aggr(aggr_size) {}
+
+
+
+}  // namespace Anorency
--- src/Environment/Environment.h
+++ src/Environment/Environment.h
@@ -3,22 +3,26 @@
 #include <concepts>
 #include <string>
 
+#include "Aggregator.h"
 #include "AnoInterface.h"
 namespace Anorency {
 
 class Environment {
  public:
   Environment() = default;
+  Environment(std::size_t aggr_size);
   ~Environment() = default;
 
   template <typename F>
     requires std::invocable<F, AnoInterface&&>
   void introduce(F&& f) {
-    f(std::move(AnoInterface(str)));
+    f(std::move(AnoInterface(str, aggr)));
   }
 
  private:
   std::string str;
+
+  Aggregator aggr;
 };
 
 }  // namespace Anorency
