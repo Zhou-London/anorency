edf18e0e707dee26436f126e553353bfad04e64d
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-10-16 11:30:30 +0100

    Adds test case for aggr integration

--- src/test.cpp
+++ src/test.cpp
@@ -4,8 +4,10 @@
 
 #include "Aggregator.h"
 #include "AnoException.h"
+#include "AnoInterface.h"
 #include "Environment.h"
 #include "Line.h"
+#include "Types.h"
 
 inline void TEST_CASE_EXCEPTION() {
   std::cout << "\nStart Test: Exception.\n";
@@ -54,9 +56,33 @@ inline void TEST_CASE_AGGREGATOR() {
   line->pop();
 }
 
+inline void TEST_CASE_AGGREGATOR_INTERFACE() {
+  std::cout << "\nStart Test: Aggregator Interface Integration.\n";
+
+  Anorency::Environment env;
+  Anorency::line_id_t id;
+  env.introduce([&](Anorency::AnoInterface&& iface) {
+    auto this_id = iface.make_line<int>();
+    id = this_id;
+    auto line = iface.get_line<int>(id);
+    line->push(10);
+
+    std::cout << "First Task: Value Pushed.\n";
+  });
+
+  env.introduce([&](Anorency::AnoInterface&& iface) {
+    auto line = iface.get_line<int>(id);
+    std::cout << line->peek() << "\n";
+    line->pop();
+
+    std::cout << "Second Task: Value Read.\n";
+  });
+}
+
 int main() {
   TEST_CASE_ENVIRONMENT_INTRODUCE();
   TEST_CASE_EXCEPTION();
   TEST_CASE_AGGREGATOR();
+  TEST_CASE_AGGREGATOR_INTERFACE();
   return 0;
 }
