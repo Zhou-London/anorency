398efeb2394b3f2729f61c484e303059bde4adc6
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-11-13 22:15:35 +0000

    Modifies AnoInterface to a template class.

--- src/AnoInterface.h
+++ src/AnoInterface.h
@@ -6,13 +6,23 @@
 #include "Types.h"
 namespace Anorency {
 
+template <typename S>
 class AnoInterface {
  public:
-  AnoInterface(std::string& str, Aggregator& aggr);
+  AnoInterface(std::string& str, Aggregator& aggr, stream_id_t uniq_stream_id)
+      : str(str), aggr(aggr), uniq_stream_id(uniq_stream_id) {}
   ~AnoInterface() = default;
 
-  void set_str(const std::string& new_str);
-  std::string get_str();
+  void set_str(const std::string& new_str) { this->str = new_str; }
+  std::string get_str() const noexcept { return this->str; }
+
+  stream_id_t get_uniq_stream_id() const noexcept {
+    return this->uniq_stream_id;
+  }
+
+  Stream<S>* get_uniq_stream() const noexcept { return this->uniq_stream; }
+
+  S read_uniq_stream() { return this->uniq_stream->read(); }
 
   template <typename T>
   stream_id_t make_stream() {
@@ -25,13 +35,17 @@ class AnoInterface {
   }
 
   template <typename T>
-  Stream<T>* get_stream(size_t index) {
+  Stream<T>* get_stream(stream_id_t index) {
     return aggr.get<T>(index);
   }
 
  private:
+  using uniq_stream_t = S;
+
   std::string& str;
   Aggregator& aggr;
+  stream_id_t uniq_stream_id;
+  Stream<S>* uniq_stream;
 };
 
 }  // namespace Anorency
