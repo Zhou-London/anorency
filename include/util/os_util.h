1f6e31f64e30414671ef2a544f36c8cd3c6deb26
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-12-20 15:49:43 +0800

    Adds util file for os&architectures-related operations.

---
+++ include/util/os_util.h
@@ -0,0 +1,5 @@
+#pragma once
+
+void cpu_relax();
+
+void set_thread_affinity(int core);
---
+++ src/util/os_util.cpp
@@ -0,0 +1,10 @@
+#include "os_util.h"
+
+#include <pthread.h>
+#include <sched.h>
+
+void cpu_relax() { asm volatile("yield" ::: "memory"); }
+
+void set_thread_affinity(int core) {
+  // TODO:
+}
