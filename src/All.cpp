edb0162ed0122c649c231eb17a3efaabd70d9b4c
zhou <zhouzhou.zhang.24@ucl.ac.uk>
2025-12-20 21:47:03 +0800

    Adds dummy cpp file regarding clangd.

---
+++ src/All.cpp
@@ -0,0 +1,7 @@
+#include "Alias.h"
+#include "MessageHelpers.h"
+#include "Stream.h"
+#include "os_util.h"
+#include "version.h"
+
+/* NOTE: This is a dummy cpp file, served for Clangd auto-completion! */
